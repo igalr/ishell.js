282654c8359069e826cef034889a03c829fb485c
iGAL Roytblat <igalr@redleafsolutions.ca>
2026-07-07 15:37:41 -0400

    declare exports

--- ts/tsup.config.ts
+++ ts/tsup.config.ts
@@ -4,14 +4,14 @@ export default defineConfig([
   {
     entry: ['src/**/*.ts', '!src/__tests__/**', '!src/local.ts', '!src/localCLI.ts'],
     format: ['cjs', 'esm'],
-    dts: false,
+    dts: true,
     minify: false,  // true
     splitting: false,
   },
   {
     entry: ['src/local.ts', 'src/localCLI.ts'],
     format: ['esm'],
-    dts: false,
+    dts: true,
     minify: false,  // true
     splitting: false,
   },
